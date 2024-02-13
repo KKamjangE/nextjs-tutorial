10220a64e352ffe47914a17778f8a0790275adf5
KKamjangE <ajm980718@gmail.com>
2024-02-13 15:59:04 +0900

    Feat: 콘솔에 state 값 출력

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -22,6 +22,7 @@ export default function EditInvoiceForm({
   const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
+  console.log(state);
   return (
     <form action={dispatch} aria-describedby="form-error">
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
