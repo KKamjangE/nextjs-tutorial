81f7abfd40de1566341713a13315dc822ed03a57
KKamjangE <ajm980718@gmail.com>
2024-02-13 15:38:14 +0900

    Feat: 서버측 검증

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -30,12 +30,21 @@ export type State = {
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(preState: State, formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
@@ -44,8 +53,11 @@ export async function createInvoice(preState: State, formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
   } catch (error) {
-    return { message: 'Database Error: Failed to Create Invoice.' };
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
   }
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
